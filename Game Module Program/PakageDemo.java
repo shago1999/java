ee5ead9d51fb46f9b5d1cda15352c58054e66a30
shago1999 <saiershgo@outlook.com>
2025-08-05 15:27:38 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   Game Module Program/Game.java
    	new file:   Game Module Program/PakageDemo.java

---
+++ Game Module Program/PakageDemo.java
@@ -0,0 +1,20 @@
+package Game ;
+
+
+
+public class PakageDemo 
+{
+    public static void main(String[] args) {
+        
+        Indoor In[] = new Indoor[3];
+        In[0] new Indoor("amit");
+        In[1] new Indoor("amit01");
+        In[2] new Indoor("amit02");
+        System.out.println("indoor players.....");
+        for(int i=0;i<In.length;i++)
+        In[i].display();
+        System.out.println("indoor players.....");
+
+        }
+
+}
