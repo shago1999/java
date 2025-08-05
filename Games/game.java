13a1551e2e92135d6bd752eb209a8d48e50d4b55
shago1999 <saiershgo@outlook.com>
2025-08-05 15:44:16 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	renamed:    Game Module Program/Game.java -> Games/Indoor.java
    	renamed:    Game Module Program/PakageDemo.java -> Games/game.java

--- Games/game.java
+++ Games/game.java
@@ -1,13 +1,13 @@
-package Game.* ;
+package Games;
 
-public class PakageDemo 
+public class game
 {
     public static void main(String[] args) {
 
         Indoor In[] = new Indoor[3];
-        In[0] new Indoor("amit");
-        In[1] new Indoor("amit01");
-        In[2] new Indoor("amit02");
+        In[0] = new Indoor("amit");
+        In[1] = new Indoor("amit01");
+        In[2] = new Indoor("amit02");
         System.out.println("indoor players.....");
         for(int i=0;i<In.length;i++)
         In[i].display();
