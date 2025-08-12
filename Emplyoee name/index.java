457168e02ad8af3ae368e6ff7d17259b4da0069c
shago1999 <saiershgo@outlook.com>
2025-08-12 15:07:33 +0530

    modified:   Emplyoee name/index.java
        class InvalidName made

--- Emplyoee name/index.java
+++ Emplyoee name/index.java
@@ -1,5 +1,16 @@
 // package Emplyoee name;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+class InvalidName extends ExecutionException
+{
+    InvalidName(String msg)
+    {
+        super(msg);
+    }
+}
 
 public class index {
-    
+
 }
